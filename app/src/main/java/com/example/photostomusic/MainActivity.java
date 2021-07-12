e80fa38d2188c88bb3eb696e6ab34d7b82df8d9d
Alberto Cortés <albert.cortes.000@gmail.com>
2021-07-12 16:18:27 -0500

    New comment on main activity
    Comment used to test github for desktop

--- app/src/main/java/com/example/photostomusic/MainActivity.java
+++ app/src/main/java/com/example/photostomusic/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Comment goes here
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
